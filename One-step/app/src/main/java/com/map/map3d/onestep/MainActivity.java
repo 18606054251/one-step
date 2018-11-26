3c624f8be5738f1de80a9c792e8741502bab602e
wyz0918 <wuyizhao2008@126.com>
2018-11-27 01:10:10 +0800

    feat:增加轨迹绘制功能

--- One-step/app/src/main/java/com/map/map3d/onestep/MainActivity.java
+++ One-step/app/src/main/java/com/map/map3d/onestep/MainActivity.java
@@ -8,31 +8,39 @@ import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.util.Log;
+import android.widget.Toast;
 
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.AMap.InfoWindowAdapter;
 import com.amap.api.maps.AMap.OnInfoWindowClickListener;
 import com.amap.api.maps.AMap.OnMapClickListener;
 import com.amap.api.maps.AMap.OnMarkerClickListener;
+import com.amap.api.maps.AMapUtils;
 import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.MapView;
 import com.amap.api.maps.model.BitmapDescriptor;
 import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.Circle;
 import com.amap.api.maps.model.CircleOptions;
 import com.amap.api.maps.model.LatLng;
 import com.amap.api.maps.model.LatLngBounds;
 import com.amap.api.maps.model.Marker;
 import com.amap.api.maps.model.MarkerOptions;
 import com.amap.api.maps.model.MyLocationStyle;
-import com.amap.api.maps.CameraUpdate;
+import com.amap.api.maps.UiSettings;
+import com.amap.api.maps.model.PolygonOptions;
+import com.amap.api.maps.model.PolylineOptions;
 import com.amap.api.services.core.AMapException;
 import com.amap.api.services.core.LatLonPoint;
 import com.amap.api.services.core.PoiItem;
@@ -41,16 +49,17 @@ import com.amap.api.services.poisearch.PoiResult;
 import com.amap.api.services.poisearch.PoiSearch;
 import com.amap.api.services.poisearch.PoiSearch.OnPoiSearchListener;
 import com.amap.api.services.poisearch.PoiSearch.SearchBound;
-import com.map.map3d.onestep.util.Constants;
 import com.map.map3d.onestep.util.ToastUtil;
 
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener ,
         OnMapClickListener, OnInfoWindowClickListener, InfoWindowAdapter, OnMarkerClickListener,
-        OnPoiSearchListener, LocationSource.OnLocationChangedListener,AMap.OnMyLocationChangeListener {
+        OnPoiSearchListener{
 
     private MapView mapView;
     private AMap aMap;
@@ -82,12 +91,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private List points = new ArrayList();//放入用户的行走点数据
     private MarkerOptions markerOption; //用户点击评论时所在点的集合
 
+    private static final String TAG = "MainActivity";
+    private LocationSource.OnLocationChangedListener mListener;
+    private AMapLocationClient mLocationClient;
+    private AMapLocationClientOption mLocationOption;
+    private AMapLocation privLocation;
+    private AMapLocation curLocation;
+    private double distance;
+    private boolean start_draw = false;
 
-    private DecimalFormat df = new DecimalFormat("#.000000");
-    private MyLocationStyle myLocationStyle;
-    private double mLocationLatitude;
-    private double mLocationLongitude;
-    private boolean isFirst = true;
+    private CircleOptions circle;
+
 
 
     @Override
@@ -98,12 +112,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mapView = (MapView) findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);// 此方法必须重写
 
+        if (aMap == null) {
+            aMap = mapView.getMap();
+
+        }
+
+        circle = new CircleOptions().
+                center(new LatLng(30,116)).
+                radius(10000000).
+                fillColor(Color.argb(55, 1, 1, 1));
+
+
+        aMap.setOnMapClickListener(this);
+        aMap.setOnMarkerClickListener(this);
+        aMap.setOnInfoWindowClickListener(this);
+        aMap.setInfoWindowAdapter(this);
+
         if (getSupportActionBar() != null){
             getSupportActionBar().hide();
         }
 
+        aMap.getUiSettings().setZoomControlsEnabled(false);
+        aMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+        aMap.setMapTextZIndex(2);
+
 
         init();
+        setup();
+
+
+
 
     }
 
@@ -114,41 +152,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * 初始化AMap对象
      */
     private void init() {
-        if (aMap == null) {
-            aMap = mapView.getMap();
-            aMap.setOnMapClickListener(this);
-            aMap.setOnMarkerClickListener(this);
-            aMap.setOnInfoWindowClickListener(this);
-            aMap.setInfoWindowAdapter(this);
-
-            TextView searchButton = (TextView) findViewById(R.id.btn_search);
-            searchButton.setOnClickListener(this);
 
 
-
-
-        }
-
+        TextView searchButton = (TextView) findViewById(R.id.btn_search);
+        searchButton.setOnClickListener(this);
         start_trace =(Button)findViewById(R.id.start_trace);
         cover = (View)findViewById(R.id.cover);
         review  =(Button)findViewById(R.id.review);
+
+
         start_trace.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                start_trace.setText(getResources().getString(R.string.end_trace));
                 switch (button_status){
+                    //点击开始记录
                     case 0:
                         start_trace.setBackgroundResource(R.drawable.end_trace);
                         start_trace.setText(getResources().getString(R.string.end_trace));
                         start_trace.setPadding(0,0,0,0);
                         button_status = 1;
-                        cover.setVisibility(View.VISIBLE);
+//                        cover.setVisibility(View.VISIBLE);
                         cover.setAlpha(0.6f);
                         review.setVisibility(View.VISIBLE);
                         review.setPadding(0,0,0,0);
+                        start_draw = true;
+                        aMap.addCircle(circle);
+
 
                         break;
+
+                        //点击结束记录
                     case 1:
                         start_trace.setText(getResources().getString(R.string.start_trace));
                         start_trace.setBackgroundResource(R.drawable.start_trace);
@@ -157,6 +191,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         cover.setVisibility(View.INVISIBLE);
                         review.setVisibility(View.INVISIBLE);
                         review.setPadding(0,0,0,0);
+                        start_draw = false;
+                        aMap.clear(true);
+
 
                         break;
 
@@ -167,22 +204,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
 
 
-        setup();
-        aMap.setOnMyLocationChangeListener(this);
-        aMap.setMyLocationStyle(myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW));
-
 
 
     }
 
     private void setup() {
 
+        UiSettings uiSettings = aMap.getUiSettings();
+        uiSettings.setZoomControlsEnabled(true);// 设置缩放按钮是否显示
+        uiSettings.setScaleControlsEnabled(true);// 设置比例尺是否显示
+        uiSettings.setRotateGesturesEnabled(true);// 设置地图旋转是否可用
+        uiSettings.setTiltGesturesEnabled(true);// 设置地图倾斜是否可用
+        uiSettings.setMyLocationButtonEnabled(false);// 设置默认定位按钮是否显示
 
-        aMap.getUiSettings().setMyLocationButtonEnabled(false);// 设置默认定位按钮是否显示
-        myLocationStyle = new MyLocationStyle();
+        MyLocationStyle myLocationStyle = new MyLocationStyle();
         aMap.setMyLocationStyle(myLocationStyle);
+        aMap.setLocationSource(mLocationSource);// 设置定位监听
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);// 设置默认定位按钮是否显示
         aMap.setMyLocationEnabled(true);// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
-
 
         mPoiDetail = (RelativeLayout) findViewById(R.id.poi_detail);
         mPoiDetail.setOnClickListener(new View.OnClickListener() {
@@ -201,6 +240,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mSearchText = (EditText)findViewById(R.id.input_edittext);
     }
 
+    private List<LatLng> createRectangle(LatLng center, double halfWidth, double halfHeight) {
+        List<LatLng> latLngs = new ArrayList<LatLng>();
+        latLngs.add(new LatLng(center.latitude - halfHeight, center.longitude - halfWidth));
+        latLngs.add(new LatLng(center.latitude - halfHeight, center.longitude + halfWidth));
+        latLngs.add(new LatLng(center.latitude + halfHeight, center.longitude + halfWidth));
+        latLngs.add(new LatLng(center.latitude + halfHeight, center.longitude - halfWidth));
+        return latLngs;
+    }
+
+
+
+
+
     /**
      * 开始进行poi搜索
      */
@@ -214,7 +266,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         query.setPageSize(20);// 设置每页最多返回多少条poiitem
         query.setPageNum(currentPage);// 设置查第一页
 
-        search_point = new LatLonPoint(mLocationLatitude,mLocationLongitude);
+        search_point = new LatLonPoint(curLocation.getLatitude(),curLocation.getLongitude());
 
 
             poiSearch = new PoiSearch(this, query);
@@ -259,6 +311,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
+
     }
 
 
@@ -306,11 +359,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                 .icon(BitmapDescriptorFactory
                                         .fromBitmap(BitmapFactory.decodeResource(
                                                 getResources(), R.mipmap.point4)))
-                                .position(new LatLng(mLocationLatitude, mLocationLongitude)));
+                                .position(new LatLng(curLocation.getLatitude(), curLocation.getLongitude())));
 
                         aMap.addCircle(new CircleOptions()
-                                .center(new LatLng(mLocationLatitude,
-                                        mLocationLongitude)).radius(5000)
+                                .center(new LatLng(curLocation.getLatitude(),
+                                        curLocation.getLongitude())).radius(5000)
                                 .strokeColor(Color.BLUE)
                                 .fillColor(Color.argb(50, 1, 1, 1))
                                 .strokeWidth(2));
@@ -453,38 +506,133 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mPoiAddress.setText(mCurrentPoi.getSnippet()+mCurrentPoi.getDistance());
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
+
+
+
+
+
+    /**
+     * 绘制运动路线
+     *
+     * @param curLocation
+     */
+    public void drawLines(AMapLocation curLocation) {
+
+        if (null == privLocation) {
+            return;
+        }
+        PolylineOptions options = new PolylineOptions();
+        //上一个点的经纬度
+        options.add(new LatLng(privLocation.getLatitude(), privLocation.getLongitude()));
+        //当前的经纬度
+        options.add(new LatLng(curLocation.getLatitude(), curLocation.getLongitude()));
+        options.width(35);
+        options.setCustomTexture(BitmapDescriptorFactory.fromResource(R.mipmap.map_alr3));
+        options.aboveMaskLayer(true);
+        aMap.addPolyline(options);
+        //距离的计算
+        distance = AMapUtils.calculateLineDistance(new LatLng(privLocation.getLatitude(),
+                privLocation.getLongitude()), new LatLng(curLocation.getLatitude(),
+                curLocation.getLongitude()));
 
     }
+    public LocationSource mLocationSource = new LocationSource() {
+        @Override
+        public void activate(OnLocationChangedListener onLocationChangedListener) {
+            mListener = onLocationChangedListener;
+            initAmapLocation();
+        }
+        @Override
+        public void deactivate() {
+            mListener = null;
+            if (mLocationClient != null) {
+                mLocationClient.stopLocation();
+                mLocationClient.onDestroy();
+            }
+            mLocationClient = null;
+        }
 
-    @Override
-    public void onMyLocationChange(Location location) {
-        if(location != null) {
-            Bundle bundle = location.getExtras();
-            if (bundle != null) {
-                mLocationLatitude = location.getLatitude();
-                mLocationLongitude = location.getLongitude();
-                mLocationLatitude = Double.valueOf(df.format(mLocationLatitude));
-                mLocationLongitude = Double.valueOf(df.format(mLocationLongitude));
 
-                if (isFirst) {
-                    if (mLocationLatitude > 0 && mLocationLongitude > 0) {
-                        CameraUpdate cu = CameraUpdateFactory.newLatLngZoom(new LatLng(mLocationLatitude, mLocationLongitude), 17);
-                        aMap.moveCamera(cu);
-                    } else {
-                        CameraUpdate cu = CameraUpdateFactory.newLatLngZoom(Constants.CHENGDU, 17);
-                        aMap.moveCamera(cu);
+
+
+        };
+
+    /**
+     * 初始化定位
+     */
+    private void initAmapLocation() {
+        //初始化定位
+        mLocationClient = new AMapLocationClient(this);
+        //设置定位回调监听
+        mLocationClient.setLocationListener(mAMapLocationListener);
+        //初始化AMapLocationClientOption对象
+        mLocationOption = new AMapLocationClientOption();
+        // 设置定位场景，目前支持三种场景（签到、出行、运动，默认无场景）
+        mLocationOption.setLocationPurpose(AMapLocationClientOption.AMapLocationPurpose.Transport);
+        //设置定位模式为AMapLocationMode.Hight_Accuracy，设备定位模式。
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Device_Sensors);
+        //设置定位间隔,单位毫秒,默认为2000ms，最低1000ms。
+        mLocationOption.setInterval(2000);
+        //设置是否返回地址信息（默认返回地址信息）
+        mLocationOption.setNeedAddress(true);
+        //单位是毫秒，默认30000毫秒，建议超时时间不要低于8000毫秒。
+        mLocationOption.setHttpTimeOut(20000);
+        mLocationOption.setMockEnable(true);
+        if (null != mLocationClient) {
+            mLocationClient.setLocationOption(mLocationOption);
+            //设置场景模式后最好调用一次stop，再调用start以保证场景模式生效
+            mLocationClient.startLocation();
+        }
+    }
+
+    /**
+     * 定位回调每1秒调用一次
+     */
+    public AMapLocationListener mAMapLocationListener = new AMapLocationListener() {
+        @Override
+        public void onLocationChanged(AMapLocation amapLocation) {
+            if (amapLocation != null) {
+                if (amapLocation.getErrorCode() == 0) {
+                    mListener.onLocationChanged(amapLocation);// 显示系统小蓝点,不写这一句无法显示到当前位置
+                    amapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
+                    Log.e(TAG, "获取经纬度集合" + privLocation);//打Log记录点是否正确
+
+                    amapLocation.getAccuracy();//获取精度信息
+                    amapLocation.getBearing();//获取方向角信息
+                    amapLocation.getSpeed();//获取速度信息  单位：米/秒
+                    amapLocation.getLocationType();//查看是什么类型的点
+                    Log.e(TAG, "获取点的类型" + amapLocation.getLocationType());
+                    if (amapLocation.getLocationType() == 1&&start_draw==true) {
+
+
+                        drawLines(amapLocation);//一边定位一边连线
+                        distance += distance;
+                        Toast.makeText(MainActivity.this, "经纬度"+distance+"KM",Toast.LENGTH_SHORT).show();
+
+
                     }
-                    isFirst = false;
-                }
+                    //获取定位时间
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    Date date = new Date(amapLocation.getTime());
+                    sdf.format(date);
+                    privLocation = amapLocation;
+                    curLocation = amapLocation;
 
-            }else {
-                    ToastUtil.show(this,"定位失败，请检查您的定位权限");
+//
+                } else {
+                    //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
+                    Log.e("AmapError", "location Error, ErrCode:"
+                            + amapLocation.getErrorCode() + ", errInfo:" + amapLocation.getErrorInfo());
                 }
-
             }
         }
+    };
+
+
+
+
+
+
 
 
     private class myPoiOverlay {
